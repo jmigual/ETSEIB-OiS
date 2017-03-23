bf20961355d13f4d0a91d8fde0a2ed32757c70f7
jmigual <j.marce.igual@gmail.com>
2017-03-23 09:00:50 +0000

    Afegit mostrar millors resultats

--- Treball/treball.py
+++ Treball/treball.py
@@ -5,12 +5,15 @@ import abc
 import logging
 import numpy
 import csv
+import getopt
+import sys
 
 
 class ComptadorEstadistic:
-    def __init__(self):
+    def __init__(self, nom_arxiu):
         self.llista_espera = []
-        csvfile = open('error.csv', 'w')
+        self.nom_arxiu = nom_arxiu
+        csvfile = open("resultats_" + self.nom_arxiu, 'w')
         self.csv = csv.writer(csvfile, delimiter=',')
         self.iteration = 1
 
@@ -25,26 +28,45 @@ class ComptadorEstadistic:
         return error
 
     def imprimir_resultats(self, estat):
+        mitjana = numpy.mean(self.llista_espera)
+        desviacio = numpy.std(self.llista_espera)
+        clients = len(self.llista_espera)
+
         logger = logging.getLogger()
         logger.info("Temps final         : {:9.4f}".format(estat.rellotge))
-        logger.info("Error final         : {:9.4f}".format(self.error()))
-        logger.info("Total de clients    : {:9.4f}".format(len(self.llista_espera)))
-        logger.info("Temps mitja d'espera: {:9.4f}".format(numpy.mean(self.llista_espera)))
+        logger.info("Total de clients    : {:9.4f}".format(clients))
+        logger.info("Temps mitjà d'espera: {:9.4f}".format(mitjana))
+        logger.info("Desviació std dades : {:9.4f}".format(desviacio))
+        logger.info("Files al CSV        : {:9.4f}".format(self.iteration))
 
-    def imprimir_estadistiques(self):
+        # Intentem aproximar les dades linealment per poder veure si el sistema es estable
+        polinomi = numpy.polyfit(range(clients), self.llista_espera, 1)
+        logger.info("Regressió a+b*x     :")
+        logger.info("    a : {:9.7f}".format(polinomi[1]))
+        logger.info("    b : {:9.7f}".format(polinomi[0]))
+
+        with open(self.nom_arxiu, mode='a') as file:
+            csv_results = csv.writer(file, delimiter=',')
+            csv_results.writerow([mitjana, desviacio, clients, polinomi[0], polinomi[1]])
+
+    def imprimir_estadistiques(self, estat):
         self.iteration += 1
-        self.csv.writerow([self.iteration, numpy.sum(self.llista_espera), self.llista_espera[-1]])
+        self.csv.writerow([self.iteration, numpy.sum(self.llista_espera), self.llista_espera[-1],
+                           len(estat.llista_persones_espera), estat.rellotge])
 
 
 class Estat:
-    def __init__(self, facturadors=12):
+    def __init__(self, nom_arxiu, facturadors=12):
         self.llista_persones_espera = []
         self.facturador_lliure = [True] * facturadors
         self.rellotge = 0
-        self.stat = ComptadorEstadistic()
+        self.stat = ComptadorEstadistic(nom_arxiu)
 
 
 class Esdeveniment(metaclass=abc.ABCMeta):
+    """Classe abstracta Esdeveniment conté els mètodes necessaris per implementar un esdeveniment
+    """
+
     def __init__(self, tipus, rellotge):
         self.tipus = tipus
         self.rellotge = rellotge
@@ -71,6 +93,13 @@ class EsdevenimentFinalitzacio(Esdeveniment):
         self.facturador = facturador
 
     def esdevenir(self, estat):
+        """
+        Implementació de la funció esdevenir per la finalització d'una facturació,
+        si hi ha més clients a la llista d'espera s'elimina un i es crea un nou
+        EsdevenimentFinalització
+        :param estat: Estat actual del sistema
+        :return: Es retorna una llista d'esdeveniments creats per aquest esdeveniment
+        """
         estat.rellotge = self.rellotge
 
         if len(estat.llista_persones_espera) <= 0:
@@ -79,9 +108,10 @@ class EsdevenimentFinalitzacio(Esdeveniment):
 
         persona = estat.llista_persones_espera.pop(0)
         estat.stat.llista_espera.append(estat.rellotge - persona)
-        estat.stat.imprimir_estadistiques()
+        estat.stat.imprimir_estadistiques(estat)
 
-        return [EsdevenimentFinalitzacio(self.rellotge + Simulacio.get_temps_facturacio(), self.facturador)]
+        return [EsdevenimentFinalitzacio(self.rellotge + Simulacio.get_temps_facturacio(),
+                                         self.facturador)]
 
 
 class EsdevenimentArribada(Esdeveniment):
@@ -101,6 +131,14 @@ class EsdevenimentArribada(Esdeveniment):
         self.nombre_passatgers = n
 
     def esdevenir(self, estat):
+        """
+        Implementació de la funció esdevenir per l'arribada de nous passatgers, si hi ha n
+        facturadors disponibles es generaran n EsdevenimentFinalitzacio i a part també es genera
+        un nou EsdevenimentArribada seguint les probabilitats especificades en quan al temps i a
+        la mida del grup que arriba
+        :param estat: Estat del sistema
+        :return: Llista amb els esdeveniments generats
+        """
         facturadors_lliures = [i for i, x in enumerate(estat.facturador_lliure) if x]
         p = min(len(facturadors_lliures), self.nombre_passatgers)
         estat.llista_persones_espera += [estat.rellotge] * (self.nombre_passatgers - p)
@@ -120,10 +158,12 @@ class Simulacio:
     ERROR_MINIM = 0.005
     simulacio_amb_maquines_autofacturacio = False
 
-    def __init__(self):
+    def __init__(self, nom_arxiu, maquines, facturadors):
+        Simulacio.simulacio_amb_maquines_autofacturacio = maquines
+        self.nom_arxiu = nom_arxiu
         self.temps_inicial = 0.0
         self.llista_esdeveniments = []
-        self.estat = Estat()
+        self.estat = Estat(nom_arxiu, facturadors=facturadors)
         self.logger = logging.getLogger()
 
         # Afegir esdeveniment inicial arribada
@@ -179,16 +219,68 @@ def configure_default_logger():
     log.addHandler(handler_f)
 
 
+def usage():
+    print("Usage: ")
+    print("python3 treball.py [-n nombre][-i nombre][-f|-m][-d]")
+    print("-i    Nombre de vegades que s'ha d'executar la simulació")
+    print("-n    Nombre facturadors o màquines facturadores")
+    print("-f    Usar agents facturadors")
+    print("-m    Usar màquines facturadores")
+    print("-d    Activar mode debug")
+
+
 def main():
+    iteracions = 1
+    maquines = False
+    facturadors = 12
+
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "n:i:dfm")
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    for o, a in opts:
+        if o == "-i":
+            iteracions = int(a)
+        elif o == "-d":
+            logging.getLogger().setLevel(logging.DEBUG)
+        elif o == "-n":
+            facturadors = int(a)
+        elif o == "-f":
+            maquines = False
+        elif o == "-m":
+            maquines = True
+        else:
+            assert False, "unhandled option"
+
     logger = logging.getLogger()
-    logger.info("Programa iniciat")
-    sim = Simulacio()
-    logger.info("Variables inicialitzades")
-    sim.executa()
+    tipus = "maquines" if maquines else "agents"
+    logger.info("Tipus facturadors    : {}".format(tipus))
+    logger.info("Nombre de facturadors: {}".format(facturadors))
+    logger.info("Nombre d'iteracions  : {}".format(iteracions))
+
+    nom_arxiu = "simulacio_{}_{}_{}.csv".format(tipus, facturadors, iteracions)
+    logger.info("Resultats a          : {}".format(nom_arxiu))
+
+    with open(nom_arxiu, mode='w') as file:
+        csv_file = csv.writer(file, delimiter=',')
+        csv_file.writerow(["Mitjana", "Desviació", "Clients", "b", "a"])
+
+    for i in range(iteracions):
+        logger.info("***************************")
+        logger.info("Simulació {} iniciada".format(i))
+        sim = Simulacio(nom_arxiu, maquines, facturadors)
+        logger.info("Variables inicialitzades")
+        sim.executa()
+        logger.info("Simulació {} finalitzada".format(i))
+    logger.info("")
+    logger.info("")
 
 
+configure_default_logger()
 if __name__ == "__main__":
-    configure_default_logger()
     try:
         main()
     except KeyboardInterrupt:
